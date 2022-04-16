fac9f751b1d55629e7f4b7ada3363d6a4deae311
umanpax <pavezinet@live.fr>
2022-04-16 19:52:13 +0200

    create first test

--- app/src/test/java/com/insta/viewmodels/SplashScreenViewModelTest.kt
+++ app/src/test/java/com/insta/viewmodels/SplashScreenViewModelTest.kt
@@ -1,40 +1,67 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.insta.utils.MockUtils
-import com.insta.ui.splashscreen.SplashScreenViewModel
 import com.insta.model.User
+import com.insta.ui.splashscreen.SplashScreenViewModel
+import com.insta.utils.AppModule
 import com.insta.utils.ApplicationConstants
-import com.insta.utils.KoinApplication
+import com.insta.utils.MockUtils
+import io.reactivex.Single
 import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.koin.core.context.startKoin
 import org.mockito.ArgumentCaptor
+import org.mockito.Mockito
 import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
 
 
 @ExperimentalCoroutinesApi
-@RunWith(JUnit4::class)
+@RunWith(MockitoJUnitRunner::class)
 class SplashScreenViewModelTest : KoinComponent {
 
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
     init {
-      KoinApplication()
+      startKoin {
+          modules(AppModule.splashViewModel,AppModule.moduleRepository)
+      }
     }
+    private val testDispatcher = StandardTestDispatcher()
     private val viewModel: SplashScreenViewModel by inject()
     private var mockUtils: MockUtils = MockUtils()
     private val userObserver: Observer<User> = mockUtils.mock()
 
+
     @Before
     fun before() {
+        Dispatchers.setMain(testDispatcher)
         viewModel.repository.liveDataUser.observeForever(userObserver)
+    }
+
+    @After
+    fun after() {
+        Dispatchers.resetMain()
     }
 
     @Test
     fun fetchUser() {
-            viewModel.repository.getUserByUserName("musicpax_",ApplicationConstants.ACCESS_KEY)
+        viewModel.repository.getUserByUserName("musicpax_",ApplicationConstants.ACCESS_KEY)
             val captor = ArgumentCaptor.forClass(User::class.java)
             captor.run {
                 verify(userObserver, times(1)).onChanged(capture())
@@ -42,4 +69,16 @@ class SplashScreenViewModelTest : KoinComponent {
             }
     }
 
+  /*  @Test
+    fun testGetCurrentUser() {
+        runBlocking {
+            val user = mock(User::class.java)
+            `when`(viewModel.repository.getUserByUserName("musicpax_",ApplicationConstants.ACCESS_KEY))
+                .thenReturn(user)
+            userViewModel.states.captureValues {
+                userViewModel.getCurrentUser()
+                assertSendsValues(100, LoadingState, UserConnected(user))
+            }
+        }
+    }*/
 }
