28d4ab7fbe05c3e70343a6d5b0e735e2340f8ca6
umanpax <pavezinet@live.fr>
2022-04-20 18:00:40 +0200

    end commit

--- app/src/androidTest/java/com/insta/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/insta/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.insta
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.insta.activities.photodetails.PhotoDetailsActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,16 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    public val rule  = ActivityScenarioRule(PhotoDetailsActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.insta", appContext.packageName)
+        onView(withId(R.id.tv_please_wait)).check(ViewAssertions.matches(ViewMatchers.withText(appContext.getString(R.string.wait_msg))))
     }
 }
